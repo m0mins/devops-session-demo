eee2b013d2cf33d19c0fe33de6e223d8504da659
momin <momin@strativ.se>
2025-10-23 16:38:33 +0600

    updated message

--- app1/main.py
+++ app1/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Welcome to DevOps Session !!")
+    return jsonify(message="Welcome to DevOps Session from App One!!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
