0e6dc3b4c5c73fe0708c92eae05658196ceeb12b
momin <momin@strativ.se>
2025-10-23 12:31:43 +0600

    updated message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="Hello from Flask via Nginx & Docker, deployed on EC2!")
+    return jsonify(message="Welcome to DevOps Session !!")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
